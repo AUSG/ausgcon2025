9ae784d32020d80b87b51f0b99b90781f246bec3
caseBread <kgu0515@gmail.com>
2025-08-06 13:07:43 +0900

    feat : 배경색 지정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import KakaoMap from "@/components/map/KakaoMap";
 export default function Home() {
   return (
     <div>
-      <KakaoMap
+      {/* <KakaoMap
         width={1200}
         height={380}
         lat={37.50359690358887}
         lng={127.04153904751364}
       />
-      <Contact />
+      <Contact /> */}
     </div>
   );
 }
