ec70790688377c518648ee71b9409ee5a7ec5532
caseBread <kgu0515@gmail.com>
2025-08-06 14:04:01 +0900

    fix : 타입이슈수정

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -1,8 +1,4 @@
-type TProps = {
-  /** */
-};
-
-const Timer = ({}: TProps) => {
+const Timer = () => {
   return (
     <div className="bg-white/20 w-[1000px] h-[300px] flex justify-center items-center ">
       타이머
