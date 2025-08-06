c63196216dd8fa65a7d18a76b121f91a1fec67ee
caseBread <kgu0515@gmail.com>
2025-08-06 17:03:58 +0900

    fix : 폰트수정

--- src/components/description/DescriptionSection.tsx
+++ src/components/description/DescriptionSection.tsx
@@ -2,12 +2,12 @@ const DescriptionSection = () => {
   return (
     <section className="relative h-[41vh] w-full bg-white bg-[url('/image/description_bg.svg')] bg-cover bg-center bg-no-repeat">
       <div className="items-left absolute top-1/2 left-1/2 flex h-fit w-[44vw] translate-x-[40px] -translate-y-1/2 flex-col text-left">
-        <p className="mb-[41px] text-2xl">
+        <p className="mb-[41px] text-3xl">
           대학생과 주니어를 위한 IT 컨퍼런스!
           <br />
           “AUSGCON”(아우쓱콘)을 소개합니다
         </p>
-        <p className="font-pretendard text-xl">
+        <p className="font-pretendard text-2xl">
           AUSGCON은 “IT에 대한 열정이 있는 사람들을 위한 교류의 장”이라는
           컨셉으로 열리는 오프라인 기술 컨퍼런스입니다. 주로 대학생 및 개발자를
           대상으로 진행되며, 3개 트랙에서 진행하는 다양한 발표 세션과 참가자
