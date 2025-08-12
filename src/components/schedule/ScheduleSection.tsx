0d0b4a62c0f950db301b4dc09c9fb56de87d90a3
caseBread <kgu0515@gmail.com>
2025-08-12 10:08:16 +0900

    fix : 사소 수정

--- src/components/schedule/ScheduleSection.tsx
+++ src/components/schedule/ScheduleSection.tsx
@@ -5,7 +5,7 @@ const ScheduleSection = () => {
     <section className="">
       <div className="mt-[120px] text-center text-white">
         <h2 className="text-4xl lg:text-6xl">SCHEDULE</h2>
-        <p className="mt-[45px] text-xl font-bold lg:text-2xl">
+        <p className="mt-[45px] text-xl lg:text-2xl">
           *더미텍스트* 아우쓱콘의 일정들을 소개합니다.
         </p>
       </div>
